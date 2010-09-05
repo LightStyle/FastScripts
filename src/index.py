8ef55aa0e73b758c5f91d411aaa4cb0bc206f9b7
Niccolo' Campolungo <damnednickix@hotmail.it>
2010-09-05 14:54:13 +0200

    Creati templates principali

--- src/index.py
+++ src/index.py
@@ -31,7 +31,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Index Page'
+	script = ""
+	style = ""
+    return render_template('index.html', **locals())
+	
+@app.route("/admin/", methods=['POST','GET'])
+def admin():
+    return 'Admin Page'
 
 if __name__ == "__main__":
     app.debug = True
