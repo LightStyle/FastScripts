d20a0de9d3ba0db67db8009d1d58aa30a856857f
Niccolo' Campolungo <damnednickix@hotmail.it>
2010-09-10 19:30:31 +0200

    Devo trovare gli errori!

--- src/index.py
+++ src/index.py
@@ -23,51 +23,37 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
 
-import gen
-
-import hashlib
 from flask import Flask, render_template, Markup, request, jsonify, escape
+import hashlib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = hashlib.sha224(__name__).hexdigest()
 
 @app.route("/")
 def index():
+	try:
+		script = Markup(request.args['script'])
+	except:
+		script = Markup("document.write('Test JavaScript<br />')")
     try:
-        script = Markup(request.args['script'])
-    except:
-        script = Markup("document.write('Test JavaScript<br />')")
-    try:
-        style = request.args['style']
-    except:
-        style = "body { background: #000; color: #fff; }"
-    return render_template('index.html', **locals())
+		style = request.args['style']
+	except:
+		style = "body { background: #000; color: #fff; }"
+	return render_template('index.html', **locals())
 
-#funzione solo di controllo, verra' rimossa in seguito
-@app.route("/s/")
-def s():
-    sk = app.config['SECRET_KEY']
-    sk = sk[::-1]
-    return 'Secret Key Lenght: {0}<br />\nSecret Key: {1}'.format(len(sk),
-                                                                  sk)
+@app.route("/generator/<gen_name>", methods=['GET', 'POST'])
+def generatore(gen_name):
+	gen_list = ['nocopy', 'support']
+	if gen_name in gen_list:
+		gen_load = __import__('modules.'+gen_name, globals(), locals(), ['generator'], -1)
+		gen = gen_load.generator()
+		output = gen.output()
 
-@app.route("/admin/")
-def admin():
-    return render_template('admin.html', **locals())
-    
-@app.route("/_nocopy/")
-def _nocopy():
-    try:
-        type = request.args['type']
-    except:
-        type = "1"
-    try:
-        alert = request.args['alert']
-    except:
-        alert = ''
-    script = gen.nocopy(type, alert)
-    return jsonify(script=script)
+		html = render_template('modules/'+gen_name+'.html', output=output)
+		return html
+
+# -------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+	app.debug = True
+	app.run('0.0.0.0')
