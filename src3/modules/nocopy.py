33113a661df44ed0f51687931257edc5a1ee4552
Niccolo' Campolungo <damnednickix@hotmail.it>
2010-09-11 19:34:43 +0200

    Completato script up-support

--- src3/modules/nocopy.py
+++ src3/modules/nocopy.py
@@ -1,11 +1,39 @@
-﻿#! -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# -------------------------------------------------------------------------
+# Modulo: Scripts Anti Copiatura
+# Guida associata: http://ffmagazine.forumfree.it/?t=36582013
+# -------------------------------------------------------------------------
+# ForumFree Magazine Fast Scripts Generator
+# Ultimo aggiornamento: 10/09/2010 20:48 - v0.1alpha
+# Copyright (C) 2010 ForumFree Magazine http://ffmagazine.forumfree.it
+#
+# Sviluppatori:
+# Niccolo` "Kurono" Campolungo <damnednickix@hotmail.it>
+# Antonio Micari <darkstyle96@gmail.com>
+# Damiano Faraone (aka Bowser) <bowser@ffmagazine.net>
+# -------------------------------------------------------------------------
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+# -------------------------------------------------------------------------
 from flask import request
 class generator:
 	__script = ''
 	__check = ''
 
 	__alert = ''
-	__select = 1
+	__select = ''
 	
 	def __init__(self):
 		try:
@@ -14,17 +42,19 @@ class generator:
 			pass
 			
 		try:
-			self.__check = str(request.args['check'])
+			self.__check = str(request.values['check'])
 		except:
 			pass
 			
 		try:
-			self.__alert = str(request.args['nocopy_message'])
+			self.__alert = str(request.values['nocopy_message'])
 		except:
 			pass
 
 		if len(self.__check) > 0:
 			self.__script = self.__load()
+		elif self.__select != '':
+			self.__script = self.__select
 		
 	def __load(self):
 		if self.__select == 1:
@@ -53,10 +83,10 @@ document.oncontextmenu=document.onselectstart=document.ondragstart=function() {r
 			return '''<script type="text/javascript">
 //Generato con il FFMag FastScripts - Script by Bowser
 var tdmessaggio = "%s";
-document.oncontextmenu=function(){ alert(tdmessaggio + "\nAdesso apparira il menu'"); return true; }
+document.oncontextmenu=function(){ alert(tdmessaggio + "\\nAdesso apparira il menu'"); return true; }
 </script>''' % self.__alert
 		else:
-			return 'ERROR'
+			return 'ERRORE'
 
 	def output(self):
 		return self.__script
--- src3/modules/support.py
+++ src3/modules/support.py
@@ -14,39 +14,39 @@ class generator:
 	__inputImage = ''
 	def __init__(self):
 		try:
-			self.__check = str(request.args['check'])
+			self.__check = str(request.values['check'])
 		except:
 			pass
 		try:
-			self.__isText = int(request.args['support_isText'])
+			self.__isText = int(request.values['support_isText'])
 		except:
 			pass
 		try:
-			self.__alert = str(request.args['support_text'])
+			self.__alert = str(request.values['support_text'])
 		except:
 			pass
 		try:
-			self.__id = str(request.args['support_id'])
+			self.__id = str(request.values['support_id'])
 		except:
 			pass
-		self.__inputText = self.__isText and '<input type="submit" value="%s" class="forminput">' % self.__alert or ''
-		self.__inputImage = (not int(isText)) and '<input type="image" src="%s" style="border: 0;">' % self.__alert or ''
+		self.__inputText = self.__isText and '<input type="submit" value="%s" class="forminput">\n' % self.__alert or ''
+		self.__inputImage = (not int(self.__isText)) and '<input type="image" src="%s" style="border: 0;">\n' % self.__alert or ''
 		if len(self.__check) > 0:
 			self.__script = self.__load()
+		else:
+			self.__script = 1 #number
 		
 	def __load(self):
-		return '''<!-- Generato con il FFMagazine FastScripts - Script by Delta & Bowser -->
+		return '''<!-- Generato con il FFMagazine FastScripts - Script by Delta e Bowser -->
 <form action="http://supporto.forumfree.it/" method="post" name="REPLIER">
 <input type="hidden" name="st" value="0">
 <input type="hidden" name="act" value="Post">
 <input type="hidden" name="f" value="26622">
 <input type="hidden" name="CODE" value="03">
 <input type="hidden" name="t" value="%s">
-%s
-<input type="hidden" name="Post" value="Up!">
-%s
-</form><br>
-<small>Ideato da Delta & Bowser © <a href="http://ffmagazine.forumfree.it/">ForumFree Magazine</a></small>''' % (self.__id, self.__inputImage, self.__inputText)
+%s<input type="hidden" name="Post" value="Up!">
+%s</form><br>
+<small>Ideato da Delta e Bowser (c) <a href="http://ffmagazine.forumfree.it/">ForumFree Magazine</a></small>''' % (self.__id, self.__inputImage, self.__inputText)
 
 	def output(self):
 		return self.__script
